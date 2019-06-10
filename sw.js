3f5e4374d719b43a1ed20e474ef8bc2a4b3f2c66
Shohei <dis.ma.magic1122@gmail.com>
2019-06-11 01:10:46 +0900

    swtest

--- sw.js
+++ sw.js
@@ -9,45 +9,10 @@ const FILES_TO_CACHE = [
   '/',
 ];
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
-  evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request)
-          .then((response) => {
-            return response || fetch(evt.request);
-          });
-    })
-  );
-});
+// CODELAB: Precache static resources here.
+evt.waitUntil(
+  caches.open(CACHE_NAME).then((cache) => {
+    console.log('[ServiceWorker] Pre-caching offline page');
+    return cache.addAll(FILES_TO_CACHE);
+  })
+);
