87b8dd40b922d9bfea826f76171dc9a65d6ce828
Shohei <dis.ma.magic1122@gmail.com>
2019-06-12 00:34:01 +0900

    swtest

--- sw.js
+++ sw.js
@@ -14,9 +14,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License
- 
+*/ 
 'use strict';
-*/
+
 // CODELAB: Update cache names any time any of the cached files change.
 const CACHE_NAME = 'static-cache-v1';
 
