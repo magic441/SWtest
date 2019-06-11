3abaa4634f2c8a3e376fbefa7079c8919f9ad78a
Shohei <dis.ma.magic1122@gmail.com>
2019-06-11 23:39:56 +0900

    swtest

--- sw.js
+++ sw.js
@@ -1,13 +1,25 @@
 
+
+'use strict';
+
+// CODELAB: Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v1';
+
+
 // CODELAB: Update cache names any time any of the cached files change.
 const FILES_TO_CACHE = [
   './index.html',
 ];
 
-// CODELAB: Precache static resources here.
-evt.waitUntil(
-  caches.open(CACHE_NAME).then((cache) => {
-    console.log('[ServiceWorker] Pre-caching offline page');
-    return cache.addAll(FILES_TO_CACHE);
-  })
-);
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+  // CODELAB: Precache static resources here.
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
