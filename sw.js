4e3770956764a75a2c877843fd8003b8589e1d5a
Shohei <dis.ma.magic1122@gmail.com>
2019-06-11 23:12:52 +0900

    swtest

--- sw.js
+++ sw.js
@@ -4,3 +4,10 @@ const FILES_TO_CACHE = [
   '/',
 ];
 
+// CODELAB: Precache static resources here.
+evt.waitUntil(
+  caches.open(CACHE_NAME).then((cache) => {
+    console.log('[ServiceWorker] Pre-caching offline page');
+    return cache.addAll(FILES_TO_CACHE);
+  })
+);
