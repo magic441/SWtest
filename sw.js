e3e381180f7f57a8bf279e9624dd843513e0a52f
Shohei <dis.ma.magic1122@gmail.com>
2019-06-11 23:48:03 +0900

    swtest

--- sw.js
+++ sw.js
@@ -22,9 +22,10 @@ const CACHE_NAME = 'static-cache-v1';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
-  './index.html'
+  '/index.html'
 ];
 
+/*
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   // CODELAB: Precache static resources here.
@@ -50,3 +51,4 @@ self.addEventListener('fetch', (evt) => {
   // CODELAB: Add fetch event handler here.
 
 });
+*/
